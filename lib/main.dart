0d4c791d203c407f28222e07dc109f8d44a5a6dc
harisyounus921 <2019cs683@student.uet.edu.pk>
2023-01-28 14:34:53 +0500

    All work complete

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        useMaterial3: true,
         brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
